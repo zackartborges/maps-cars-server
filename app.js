cb49c262afebea0f1d1e53aad2b9966166026cf5
Zack Borges <zackartborges@gmail.com>
2021-10-27 16:10:57 -0400

    gets server to display body on expres-backend route

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const Express = require("express")();
-const Http = require("http").Server(Express);
-const Socketio = require("socket.io")(Http);
-const markers = [];
+const express = require("express"); //Line 1
+const app = express(); //Line 2
+const port = process.env.PORT || 5000; //Line 3
+const http = require("http").Server(express);
+const socketio = require("socket.io")(http);
 
-Socketio.on("connection", (socket) => {
-  for (let i = 0; i < markers.length; i++) {
-    socket.emit("marker", markers[i]);
-  }
-  socket.on("marker", (data) => {
-    markers.push(data);
-    Socketio.emit("marker", data);
-  });
-});
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-Http.listen(3001, () => {
-  console.log("Listening at :3001...");
-});
+// create a GET route
+app.get("/express_backend", (req, res) => {
+  //Line 9
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
+}); //Line 11
